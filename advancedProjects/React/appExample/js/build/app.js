10194385ad22513a3264e31a1c682fc8c4c0ba44
bogdankf1 <bogdankf1@gmail.com>
2017-10-24 22:18:11 +0300

    Finished example of news app, using eventEmitter

--- advancedProjects/React/appExample/js/build/app.js
+++ advancedProjects/React/appExample/js/build/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -12,170 +10,131 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
 //array of default news
 var my_news = [{
-  author: 'Саша	Печкин',
-  text: 'В	четчерг,	четвертого	числа...',
-  bigText: 'в	четыре	с	четвертью	часа	четыре	чёрненьких	чумазеньких	чертёнка	чертили чёрными	чернилами	чертёж.'
+  author: 'Саша Печкин',
+  text: 'В четчерг, четвертого числа...',
+  bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
 }, {
-  author: 'Просто	Вася',
-  text: 'Считаю,	что	$	должен	стоить	35	рублей!',
-  bigText: 'А	евро	42!'
+  author: 'Просто Вася',
+  text: 'Считаю, что $ должен стоить 35 рублей!',
+  bigText: 'А евро 42!'
 }, {
   author: 'Гость',
-  text: 'Бесплатно.	Скачать.	Лучший	сайт	-	http://localhost:3000',
-  bigText: 'На	самом	деле	платно,	просто	нужно	прочитать	очень	длинное	лицензионное соглашение'
+  text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+  bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
 }];
 
-//news item with text and author
+var Article = _react2.default.createClass({
+  displayName: 'Article',
 
-var Article = function (_React$Component) {
-  _inherits(Article, _React$Component);
+  // propTypes: {
+  //   data: React.PropTypes.shape({
+  //     author: React.PropTypes.string.isRequired,
+  //     text: React.PropTypes.string.isRequired,
+  //     bigText: React.PropTypes.string.isRequired
+  //   })
+  // },
+  getInitialState: function getInitialState() {
+    return {
+      visible: false
+    };
+  },
+  readmoreClick: function readmoreClick(e) {
+    e.preventDefault();
+    this.setState({ visible: true });
+  },
+  render: function render() {
+    var author = this.props.data.author,
+        text = this.props.data.text,
+        bigText = this.props.data.bigText,
+        visible = this.state.visible;
 
-  function Article() {
-    _classCallCheck(this, Article);
-
-    return _possibleConstructorReturn(this, (Article.__proto__ || Object.getPrototypeOf(Article)).apply(this, arguments));
+    return _react2.default.createElement(
+      'div',
+      { className: 'article' },
+      _react2.default.createElement(
+        'p',
+        { className: 'news__author' },
+        author,
+        ':'
+      ),
+      _react2.default.createElement(
+        'p',
+        { className: 'news__text' },
+        text
+      ),
+      _react2.default.createElement(
+        'a',
+        { href: '#',
+          onClick: this.readmoreClick,
+          className: 'news__readmore ' + (visible ? 'none' : '') },
+        '\u041F\u043E\u0434\u0440\u043E\u0431\u043D\u0435\u0435'
+      ),
+      _react2.default.createElement(
+        'p',
+        { className: 'news__big-text ' + (visible ? '' : 'none') },
+        bigText
+      )
+    );
   }
+});
 
-  _createClass(Article, [{
-    key: 'render',
-    value: function render() {
-      var author = this.props.data.author,
-          text = this.props.data.text,
-          bigText = this.props.data.bigText;
+var News = _react2.default.createClass({
+  displayName: 'News',
 
-      return _react2.default.createElement(
-        'div',
-        { className: 'Article' },
-        _react2.default.createElement(
-          'p',
-          { className: 'news-author' },
-          author,
-          ':'
-        ),
-        _react2.default.createElement(
-          'p',
-          { className: 'news-text' },
-          text
-        ),
-        _react2.default.createElement(
-          'p',
-          { className: 'news-bigText' },
-          bigText
-        )
-      );
-    }
-  }]);
+  propTypes: {
+    data: _react2.default.PropTypes.array.isRequired
+  },
+  render: function render() {
+    var data = this.props.data;
+    var newsTemplate;
 
-  return Article;
-}(_react2.default.Component);
-
-//block of Article components
-
-
-var News = function (_React$Component2) {
-  _inherits(News, _React$Component2);
-
-  function News() {
-    _classCallCheck(this, News);
-
-    return _possibleConstructorReturn(this, (News.__proto__ || Object.getPrototypeOf(News)).apply(this, arguments));
-  }
-
-  _createClass(News, [{
-    key: 'render',
-    value: function render() {
-      var data = this.props.data;
-      var newsTemplate;
-      if (data.length > 0) {
-        newsTemplate = data.map(function (item, index) {
-          return _react2.default.createElement(
-            'div',
-            { key: index },
-            _react2.default.createElement(Article, { data: item })
-          );
-        });
-      } else {
-        newsTemplate = _react2.default.createElement(
-          'p',
-          null,
-          'Any news to date'
+    if (data.length > 0) {
+      newsTemplate = data.map(function (item, index) {
+        return _react2.default.createElement(
+          'div',
+          { key: index },
+          _react2.default.createElement(Article, { data: item })
         );
-      }
-      return _react2.default.createElement(
-        'div',
-        { className: 'News' },
-        'Component News',
-        newsTemplate
+      });
+    } else {
+      newsTemplate = _react2.default.createElement(
+        'p',
+        null,
+        '\u041A \u0441\u043E\u0436\u0430\u043B\u0435\u043D\u0438\u044E \u043D\u043E\u0432\u043E\u0441\u0442\u0435\u0439 \u043D\u0435\u0442'
       );
     }
-  }]);
 
-  return News;
-}(_react2.default.Component);
-
-//block for case, when there are any news
-
-
-var Comments = function (_React$Component3) {
-  _inherits(Comments, _React$Component3);
-
-  function Comments() {
-    _classCallCheck(this, Comments);
-
-    return _possibleConstructorReturn(this, (Comments.__proto__ || Object.getPrototypeOf(Comments)).apply(this, arguments));
+    return _react2.default.createElement(
+      'div',
+      { className: 'news' },
+      newsTemplate,
+      _react2.default.createElement(
+        'strong',
+        { className: 'news__count ' + (data.length > 0 ? '' : 'none') },
+        '\u0412\u0441\u0435\u0433\u043E \u043D\u043E\u0432\u043E\u0441\u0442\u0435\u0439: ',
+        data.length
+      )
+    );
   }
+});
 
-  _createClass(Comments, [{
-    key: 'render',
-    value: function render() {
-      return _react2.default.createElement(
-        'div',
-        { className: 'Comments' },
-        'Component Comments'
-      );
-    }
-  }]);
+var App = _react2.default.createClass({
+  displayName: 'App',
 
-  return Comments;
-}(_react2.default.Component);
-
-//full app block
-
-
-var App = function (_React$Component4) {
-  _inherits(App, _React$Component4);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+  render: function render() {
+    return _react2.default.createElement(
+      'div',
+      { className: 'app' },
+      _react2.default.createElement(
+        'h3',
+        null,
+        '\u041D\u043E\u0432\u043E\u0441\u0442\u0438'
+      ),
+      _react2.default.createElement(News, { data: my_news })
+    );
   }
+});
 
-  _createClass(App, [{
-    key: 'render',
-    value: function render() {
-      return _react2.default.createElement(
-        'div',
-        { className: 'App' },
-        'Component App',
-        _react2.default.createElement(News, { data: my_news }),
-        _react2.default.createElement(Comments, null)
-      );
-    }
-  }]);
-
-  return App;
-}(_react2.default.Component);
-
-//render for full app
-
-
-_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('app'));
+_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('root'));
--- advancedProjects/React/appExample/js/source/app.js
+++ advancedProjects/React/appExample/js/source/app.js
@@ -1,93 +1,200 @@
-'use strict';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//array of default news
-var	my_news	=	[
-		{
-				author:	'Саша	Печкин',
-				text:	'В	четчерг,	четвертого	числа...',
-				bigText:	'в	четыре	с	четвертью	часа	четыре	чёрненьких	чумазеньких	чертёнка	чертили чёрными	чернилами	чертёж.'
-		},
-		{
-				author:	'Просто	Вася',
-				text:	'Считаю,	что	$	должен	стоить	35	рублей!',
-				bigText:	'А	евро	42!'
-		},
-		{
-				author:	'Гость',
-				text:	'Бесплатно.	Скачать.	Лучший	сайт	-	http://localhost:3000',
-				bigText:	'На	самом	деле	платно,	просто	нужно	прочитать	очень	длинное	лицензионное соглашение'
-		}
+//default news array
+var my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+  }
 ];
 
-//news item with text and author
-class Article extends React.Component {
-		render()	{
-			var	author	=	this.props.data.author,
-					text	=	this.props.data.text,
-					bigText = this.props.data.bigText;
+window.ee	=	new	EventEmitter();
 
-			return (
-				<div className="Article">
-						<p className="news-author">{author}:</p>
-						<p className="news-text">{text}</p>
-						<p className="news-bigText">{bigText}</p>
-				</div>
-			)
-		}
-}
+//Article component
+var Article = React.createClass({
+  propTypes: {
+    data: React.PropTypes.shape({
+      author: React.PropTypes.string.isRequired,
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired
+    })
+  },
 
-//block of Article components
-class News extends React.Component {
-  render() {
-    var data = this.props.data;
-    var newsTemplate
-    if(data.length > 0) {
-     newsTemplate = data.map(function (item, index) {
-      return (
-        <div key={index}>
-          <Article data={item}/>
-        </div>
-      );
-    });
-    } else {
-       newsTemplate = <p>Any news to date</p>;
-    }
+  getInitialState: function() {
+    return {
+      visible: false
+    };
+  },
+
+  readmoreClick: function(event) {
+    event.preventDefault();
+    this.setState({visible: true});
+  },
+
+  render: function() {
+    var author = this.props.data.author,
+        text = this.props.data.text,
+        bigText = this.props.data.bigText,
+        visible = this.state.visible;
+
     return (
-      <div className="News" >
-        Component News{newsTemplate}
-      </div>
-    );
-  }
-}
-
-//block for case, when there are any news
-class Comments extends React.Component {
-  render() {
-    return (
-      <div className="Comments">
-        Component Comments
-      </div>
-    );
-  }
-}
-
-//full app block
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        Component App
-        <News data={my_news}/>
-        <Comments />
+      <div className='Article'>
+        <p className='news-author'>{author}:</p>
+        <p className='news-text'>{text}</p>
+        <a href="#"
+          onClick={this.readmoreClick}
+          className={'news-readmore ' + (visible ? 'none': '')}>
+          Подробнее
+        </a>
+        <p className={'news-bigText ' + (visible ? '': 'none')}>{bigText}</p>
       </div>
     )
   }
-}
+});
 
-//render for full app
+//TestInput component
+var Add = React.createClass({
+  getInitialState:	function()	{
+			return	{
+					agreeNotChecked: true,
+          authorIsEmpty: true,
+          textIsEmpty: true
+			};
+	},
+	componentDidMount:	function()	{
+			ReactDOM.findDOMNode(this.refs.author).focus();
+	},
+  onBtnClickHandler:	function(e)	{
+		e.preventDefault();
+		var	author	=	ReactDOM.findDOMNode(this.refs.author).value;
+		var	addText	=	ReactDOM.findDOMNode(this.refs.text);
+
+    var text = addText.value;
+		var item = [{
+      author: author,
+      text: text,
+      bigtext: "..."
+    }];
+    window.ee.emit("News.add", item);
+    addText.value = "";
+  },
+  onCheckRuleClick:	function()	{
+      this.setState({agreeNotChecked:	!this.state.agreeNotChecked});
+	},
+  onFieldChange: function (fieldName, e) {
+    if (e.target.value.trim().length	>	0)	{
+			 this.setState({[""+fieldName]:false})
+		}	else {
+			 this.setState({[""+fieldName]:true})
+		}
+  },
+	render: function () {
+    var	agreeNotChecked	=	this.state.agreeNotChecked,
+				authorIsEmpty	=	this.state.authorIsEmpty,
+				textIsEmpty	=	this.state.textIsEmpty;
+		return (
+			<form className="Add">
+				<input
+					type="text"
+					className="add-author"
+					placeholder="Your name"
+          onChange={this.onFieldChange.bind(this, "authorIsEmpty")}
+					defaultValue=""
+					ref="author"
+				/>
+				<textarea
+					className="add-text"
+					placeholder="News text"
+          onChange={this.onFieldChange.bind(this, "textIsEmpty")}
+					defaultValue=""
+					ref="text"
+				></textarea>
+				<label className="add-checkrule">
+          <input type="checkbox"
+    			 onChange={this.onCheckRuleClick}
+           ref="checkrule"/>I agree with rules
+        </label>
+				<button
+        onClick={this.onBtnClickHandler}
+        className="add-btn"
+				 ref="add_button"
+         disabled={agreeNotChecked	||	authorIsEmpty	||	textIsEmpty}
+				>Add</button>
+			</form>
+			);
+	 }
+});
+
+
+//News component
+var News = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+
+  render: function() {
+    var data = this.props.data;
+    var newsTemplate;
+
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index) {
+        return (
+          <div key={index}>
+            <Article data={item} />
+          </div>
+        )
+      })
+    } else {
+      newsTemplate = <p>К сожалению новостей нет</p>
+    }
+
+    return (
+      <div className='News'>
+        {newsTemplate}
+        <strong className={'news-count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+      </div>
+    );
+  }
+});
+
+//App component
+var App = React.createClass({
+  getInitialState: function () {
+    return {
+      news:my_news
+    };
+  },
+  componentDidMount: function () {
+    var self = this;
+    window.ee.addListener("News.add", function (item) {
+      var nextNews = item.concat(self.state.news);
+      self.setState({news:nextNews});
+    });
+  },
+  componentWillUmount: function () {
+    window.ee.removeListener("News.add");
+  },
+  render: function() {
+    return (
+      <div className='App'>
+        <h3>Новости</h3>
+				<Add />
+        <News data={this.state.news} />
+      </div>
+    );
+  }
+});
+
+//Render full app
 ReactDOM.render(
   <App />,
   document.getElementById('app')
